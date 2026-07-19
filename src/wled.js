531ce27b195b1dec476878e157c3ecb8c3bc3bd3
Tony Vo <tonyvo30@gmail.com>
2026-07-19 14:13:13 -0500

    refactor: remove hardcoded device IPs; route WLED I/O through HA

--- src/wled.js
+++ src/wled.js
@@ -1,17 +1,21 @@
 export async function updateWLED(card) {
 
- const wled =
-   card.getWledInfo(
-     card.config.entity
-   );
+ // The entity->ip (and its segments) mapping lives in the
+ // Home Assistant script "send wled with cct". We just hand
+ // it the entity and colour values; HA resolves the device.
 
+ await card.hass.callService(
+   "script",
+   "send_wled_with_cct",
+   {
+     entity_id: card.config.entity,
+     bri: card.bri,
+     r: card.r,
+     g: card.g,
+     b: card.b,
+     w: card.w,
+     cct: card.cct
+   }
+ );
 
- const response =
-   await fetch(
-    `http://${wled.ip}/json/state`
-   );
-
-
- const state =
-   await response.json();
 }
